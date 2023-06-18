40faec831eb7ade5f61f66fb0bdc1d997b686b8d
Samuel Einheri <Samuel.einheri@gmail.com>
2023-06-18 20:44:53 +0200

    Added some comments in the "Corpse" script

--- source/Corpse/Corpse.gd
+++ source/Corpse/Corpse.gd
@@ -18,12 +18,23 @@ func _physics_process(delta: float) -> void:
 		velocity.y = min(velocity.y, max_speed.y)
 		
 	var collision_info := move_and_collide(velocity)
+	
 	if collision_info:
 		swap_to_static_body()
 	
 
+# We probably should use the "swap_to_static_body()" function in some way.
+# Maybe have some form of "check" that: 
+	# if player collided with Vertical Spike:
+		# static_body_2d.global_position = vertical_spike.add_child(Corpse).postion
+# Vertical Spikes should probably be in a "group" & then the the "Corpse",
+# should somehow detect the group
+
 # Swaps the KinematicBody2D to a StaticBody2D
 func swap_to_static_body() -> void:
+#	if is_in_group("VerticalSpike"):
+#		print("floats")
+	
 	var static_body_2d := StaticBody2D.new()
 	# If you want to have multiple layers/masks, Check the Collision/Mask values & add them together.
 	static_body_2d.collision_layer = 2 # That value should be added here.
