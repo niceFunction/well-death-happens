ab08465b03bbf839887608607fe0cc5aef3d063a
Samuel Einheri <Samuel.einheri@gmail.com>
2023-07-08 17:14:26 +0200

    Small steps

--- source/Levels/Corpse_Storage.gd
+++ source/Levels/Corpse_Storage.gd
@@ -4,19 +4,22 @@ extends Node2D
 onready var corpses_parent := $Corpses
 onready var player := $Player setget _set_player
 
+func _ready() -> void:
+	_set_player(player)
+
 func _set_player(player):
 	if player.has_node("Corpse_Spawner"):
 		player.get_node("Corpse_Spawner").connect(
-			"corpse_spawned", self, "_on_Corpse_Spawner_corpse_spawned"
+			"corpse_created", self, "_on_Corpse_Spawner_corpse_spawned"
 		)
-		player.connect("tree_exiting")	
+		player.connect("tree_exiting", self,"_on_Player_tree_exiting")
 
 func _on_Corpse_Spawner_corpse_spawned(corpse):
 	corpses_parent.add_child(corpse)
 
 func _on_Player_tree_exiting(corpse):
 	player.get_node("Corpse_Spawner").disconnect(
-		"corpse_spawned", self, "_on_Corpse_Spawner_corpse_spawned"
+		"corpse_created", self, "_on_Corpse_Spawner_corpse_spawned"
 	)
 
 #func _ready() -> void:
