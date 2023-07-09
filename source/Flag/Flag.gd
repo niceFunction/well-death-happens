168a05b72c47bff76d7f23adb5c59c33b4ce2375
Samuel Einheri <Samuel.einheri@gmail.com>
2023-07-09 10:59:42 +0200

    Added a Signal connection in Flag

--- source/Flag/Flag.gd
+++ source/Flag/Flag.gd
@@ -12,8 +12,8 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node) -> void:
-	teleport()
 	emit_signal("flag_touched", body)
+	print("Player in Flag")
 
 func _get_configuration_warning() -> String:
 	return "next_scene needs a Scene/Level to function!" if not next_scene else ""
@@ -23,3 +23,6 @@ func teleport() -> void:
 	# use the yield(animation_player, "teleport_animation_finished") to wait before transitioning
 	get_tree().change_scene_to(next_scene)
 
+
+func _on_Flag_touched(player) -> void:
+	pass # Replace with function body.
