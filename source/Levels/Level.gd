8290d38c958c648cd0a7df58b48f7ce30c699efa
Joe <54695266+jdboris@users.noreply.github.com>
2023-07-10 07:25:49 -0400

    Disconnect Player corpse spawner by nullifying player property rather than relying on tree_exiting

--- source/Levels/Level.gd
+++ source/Levels/Level.gd
@@ -7,26 +7,29 @@ onready var player := $Player setget _set_player
 func _ready() -> void:
 	_set_player(player)
 
-func _set_player(player):
-	if player.has_node("Corpse_Spawner"):
-		player.get_node("Corpse_Spawner").connect(
+func _set_player(new_player):
+	if player:
+		player.get_node("Corpse_Spawner").disconnect(
 			"created_corpse", self, "_on_Corpse_Spawner_corpse_spawned"
 		)
-		player.connect("tree_exiting", self,"_on_Player_tree_exiting")
+	
+	if new_player != null and new_player.has_node("Corpse_Spawner"):
+		new_player.get_node("Corpse_Spawner").connect(
+			"created_corpse", self, "_on_Corpse_Spawner_corpse_spawned"
+		)
+	
+	player = new_player
 
 func _on_Corpse_Spawner_corpse_spawned(corpse):
 	corpses_parent.add_child(corpse)
-
-func _on_Player_tree_exiting():
-	player.get_node("Corpse_Spawner").disconnect(
-		"created_corpse", self, "_on_Corpse_Spawner_corpse_spawned"
-	)
 
 func change_to_level(next_level, player):
 	var current_level = self
 	current_level.call_deferred("remove_child", player)
 	
 	var next_level_instance = next_level.instance()
+	current_level.player = null
+	next_level_instance.player = player
 	next_level_instance.call_deferred("add_child",player)
 
 	var main = get_parent()
