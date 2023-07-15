9b23f54fe06df4b3384ae7ad21bd27f3575763dd
Samuel Einheri <Samuel.einheri@gmail.com>
2023-07-15 21:35:38 +0200

    Added a method for adding a Spawn Point to a spawn_point export variable

--- source/Levels/Level.gd
+++ source/Levels/Level.gd
@@ -1,10 +1,18 @@
+tool
 # This script should be applied to EVERY level that needs it.
 extends Node2D
+
+# For adding a Spawn Point to the level
+export (NodePath) var spawn_point = null # Remember to turn off visibility on SpawnPoint.
+var target_spawn_point
 
 onready var corpses_parent := $Corpses
 onready var player := $Player setget _set_player
 
 func _ready() -> void:
+	target_spawn_point = get_node(spawn_point)
+	print("Spawn Point position: " + str(target_spawn_point.global_position))
+	
 	_set_player(player)
 
 func _set_player(new_player):
@@ -30,9 +38,11 @@ func change_to_level(next_level, player):
 	var next_level_instance = next_level.instance()
 	current_level.player = null
 	next_level_instance.call_deferred("add_child",player)
-	
-	
+
 	var main = get_parent()
 	main.call_deferred("remove_child", current_level)
 	main.call_deferred("add_child", next_level_instance)
 
+# Used to remind the Developer that a SpawnPoint is needed for spawn_point export.
+func _get_configuration_warning() -> String:
+	return "spawn_point export needs a SpawnPoint to function!" if not spawn_point else ""
