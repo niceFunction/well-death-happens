2709d374363d0d6636f2cf5974d2277c1b9915b8
Joe <54695266+jdboris@users.noreply.github.com>
2023-08-07 09:24:35 -0400

    Set the Player's position to the Level's spawn_point on level change

--- source/Levels/Level.gd
+++ source/Levels/Level.gd
@@ -4,21 +4,13 @@ extends Node2D
 
 # For adding a Spawn Point to the level
 export (NodePath) var spawn_point = null # Remember to turn off visibility on SpawnPoint.
-var target_spawn_point
 
 onready var corpses_parent := $Corpses
 onready var player := $Player setget _set_player
 
 func _ready() -> void:
-	#target_spawn_point = get_node(spawn_point)
-	#print("Spawn Point position: " + str(target_spawn_point.global_position))
-	
 	# See further below for needed fix for respawn on death issue.
-	set_first_spawn_position()
 	_set_player(player)
-
-func _process(delta: float) -> void:
-	update_spawn_point_position()
 
 func _set_player(new_player):
 	if player and player.get_node("Corpse_Spawner").is_connected("created_corpse", self, "_on_Corpse_Spawner_corpse_spawned"):
@@ -48,20 +40,9 @@ func change_to_level(next_level, player):
 	var main = get_parent()
 	main.call_deferred("remove_child", current_level)
 	main.call_deferred("add_child", next_level_instance)
+	
+	player.global_position = next_level_instance.get_node(next_level_instance.spawn_point).global_position
 
 # Used to remind the Developer that a SpawnPoint is needed for spawn_point export.
 func _get_configuration_warning() -> String:
 	return "spawn_point export needs a SpawnPoint to function!" if not spawn_point else ""
-
-# Sets the Player's Spawn Point position for the first time.
-func set_first_spawn_position() -> void:
-	target_spawn_point = get_node(spawn_point)
-	print("Spawn Point position: " + str(target_spawn_point.global_position))
-	player.global_position = target_spawn_point.global_position
-
-# "Updates" the Player's Spawn Point position when the Player dies.
-func update_spawn_point_position() -> void:
-	#target_spawn_point = get_node(spawn_point) # This one is maybe unneeded?
-	# When the player spawns, set it to the "SpawnPoint"
-	if player.state_machine.state.name == "Spawn":
-		player.global_position = target_spawn_point.global_position
