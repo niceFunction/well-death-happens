cd2e270c1318c8731d9fca4bfdc74565cd3c9d10
Samuel Einheri <Samuel.einheri@gmail.com>
2023-08-16 13:52:12 +0200

    Added a body exit signal to Info Board

--- source/Other/InfoBoard/InfoBoard.gd
+++ source/Other/InfoBoard/InfoBoard.gd
@@ -4,3 +4,7 @@ extends Area2D
 
 func _on_Player_body_entered(body: Node) -> void:
 	pass # Replace with function body.
+
+
+func _on_Player_body_exited(body: Node) -> void:
+	pass # Replace with function body.
