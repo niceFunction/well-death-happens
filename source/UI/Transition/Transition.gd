685c4fbfca426d4d0f430b0b08377bccf1ed5817
Samuel Einheri <Samuel.einheri@gmail.com>
2023-08-21 17:20:50 +0200

    Removed a variable

--- source/UI/Transition/Transition.gd
+++ source/UI/Transition/Transition.gd
@@ -2,15 +2,12 @@ extends CanvasLayer
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var player_has_collided := false
-
 func _ready() -> void:
 	#animation_player.play("REF_transition_out")
 	animation_player.play("transition_into_level")
 
 func _process(delta: float) -> void:
-	if player_has_collided == true:
-		transition_out_of_level()
+	pass
 
 func transition_into_level():
 	#animation_player.connect("animation_finished", self, "transition_into_level")
@@ -21,4 +18,4 @@ func transition_out_of_level():
 	animation_player.play("transition_out_of_level")
 
 func _on_Flag_player_collided() -> void:
-	player_has_collided = true
+	print("banana")
