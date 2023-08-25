6cd5c561ad3b26688116c287d7ffc97d4940ddf1
Samuel Einheri <Samuel.einheri@gmail.com>
2023-08-25 16:39:09 +0200

    Added some code to the Main Menu

--- source/Levels/Menus/MainMenu.gd
+++ source/Levels/Menus/MainMenu.gd
@@ -4,3 +4,15 @@ onready var main_menu_animation_player: AnimationPlayer = $MainMenu_Anim_Skin/Ma
 
 func _ready() -> void:
 	main_menu_animation_player.play("MainMenu_Anim")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Confirm"):
+		main_menu_animation_player.play("MainMenu_ExitAnim")
+	
+	if event.is_action_pressed("Exit"):
+		get_tree().quit()
+
+
+func _on_MainMenu_animation_finished(anim_name: String) -> void:
+	if anim_name == "MainMenu_ExitAnim":
+		get_tree().change_scene("res://source/Levels/Main.tscn")
